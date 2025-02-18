c74e1598191e85663a353965c2a2873293e8c3eb
Ramukothoju <kothojuramu11@gmail.com>
2025-02-18 10:09:39 -0500

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HairClinic.Models
+{
+    public class Login
+    {
+        [Required]
+        
+        [RegularExpression("[A-Za-z0-9-._@+]*")]
+        
+        public string UserId { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$_-])(?=\S+$).{8,16}", ErrorMessage = "Password  Format: < br /> -No spaces.< br /> -Minimum 1 numeric.< br /> -Minimum 1 upper case &lower case alphabet.< br /> -Minimum 1, any of these Special characters: -, _, @, #, $.<br />-Should be ranging between 8 - 16 chars.")]
+
+        public string password { get; set; }    
+
+        public string returnUrl {  get; set; }
+    }
+}
---
+++ SqlDal.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace HairClinic.Models
+{
+    public class SqlDal: ISqlDal
+    {
+        HairStudioEntities context = new HairStudioEntities();
+
+        public Customer customre(int id)
+        {
+            Customer cust = context.Customers.Find(id);
+            return cust;
+        }
+        public void Client_Create(Customer customer)
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+
+        }
+
+        public void Client_Update(Customer customer)
+        {
+            context.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+       
+        public void SiginData(Singin entry)
+        {
+            context.Singins.Add(entry);
+            context.SaveChanges();
+        }
+
+        public void insertEQ(Enqiry eq)
+        {
+            context.Enqiries.Add(eq);
+            context.SaveChanges();
+
+            Remaider remaider = new Remaider();
+
+            remaider.EnqId = eq.Id;
+            remaider.name = eq.Name;
+            remaider.Phone = eq.Phone;
+            remaider.Gender = eq.Gender;
+            remaider.Date = eq.AvaliDate;
+            remaider.Feedback = eq.Discription;
+
+            context.Remaiders.Add(remaider);
+            context.SaveChanges();
+
+
+        }
+
+        public void eqremderdate(Remaider re)
+        {
+            context.Remaiders.Add(re);
+            context.SaveChanges();
+        }
+
+        public void insertEQTest(Enqiry eq)
+        {
+            //
+        }
+    }
+
+    public interface ISqlDal
+    {
+         void insertEQTest(Enqiry eq);
+    }
+}
